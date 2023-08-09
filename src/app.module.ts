1a21778fe571db34c26dcb3a9e3450658ab4f67e
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-09 16:38:18 -0400

    feat: ✨ init module moviments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service'
 import { EmployeesModule } from './employees/employees.module'
 import { CompaniesModule } from './companies/companies.module'
 import { TabletsCompaniesModule } from './tablets-companies/tablets-companies.module'
+import { MovementsModule } from './movements/movements.module'
 
 @Module({
-	imports: [EmployeesModule, CompaniesModule, TabletsCompaniesModule],
+	imports: [
+		EmployeesModule,
+		CompaniesModule,
+		TabletsCompaniesModule,
+		MovementsModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
