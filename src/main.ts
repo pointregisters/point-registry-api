1ec0101a2f74fde90f9c0c4cf3946cd3c01a726d
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-09 17:44:23 -0400

    chore(deps): 🔗 add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	)
+	app.enableCors()
+
+	await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000)
 }
 bootstrap()
