88374a574f4b5ddf57a2b5bd51c2172b06c3631f
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-09 15:50:16 -0400

    feat: ✨ init module company

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EmployeesModule } from './employees/employees.module'
+import { CompaniesModule } from './companies/companies.module'
 
 @Module({
-	imports: [EmployeesModule],
+	imports: [EmployeesModule, CompaniesModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
