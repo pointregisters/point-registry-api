2824817b24eeba1f9fa067f6e060a8f927013fda
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-09 17:24:42 -0400

    refactor: ♻️ typing return from endpoints

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -10,33 +10,37 @@ import {
 import { CompaniesService } from './companies.service'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { UpdateCompanyDto } from './dto/update-company.dto'
+import { Company } from './entities/company.entity'
 
 @Controller('companies')
 export class CompaniesController {
 	constructor(private readonly companiesService: CompaniesService) {}
 
 	@Post()
-	create(@Body() createCompanyDto: CreateCompanyDto) {
-		return this.companiesService.create(createCompanyDto)
+	async create(@Body() createCompanyDto: CreateCompanyDto): Promise<Company> {
+		return await this.companiesService.create(createCompanyDto)
 	}
 
 	@Get()
-	findAll() {
-		return this.companiesService.findAll()
+	async findAll(): Promise<Company[]> {
+		return await this.companiesService.findAll()
 	}
 
 	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.companiesService.findOne(+id)
+	async findOne(@Param('id') id: string): Promise<Company> {
+		return await this.companiesService.findOne(+id)
 	}
 
 	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-		return this.companiesService.update(+id, updateCompanyDto)
+	async update(
+		@Param('id') id: string,
+		@Body() updateCompanyDto: UpdateCompanyDto
+	): Promise<void> {
+		return await this.companiesService.update(+id, updateCompanyDto)
 	}
 
 	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.companiesService.remove(+id)
+	async remove(@Param('id') id: string): Promise<void> {
+		return await this.companiesService.remove(+id)
 	}
 }
