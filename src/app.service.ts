328c27b440293a8a579dea8ff7ee8a27edc3a173
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2024-05-06 01:55:38 -0400

    build: 📦️ change version

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
 	getStatus(): string {
-		return 'Pointregisters - API! - Running successfully v.1.1.0'
+		return 'Pointregisters - API! - Running successfully v.1.1.1'
 	}
 }
